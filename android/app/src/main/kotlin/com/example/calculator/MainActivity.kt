f77242110ed5bc3389907f050db4791c3a0f5a85
Felipe Sales <soufeliposales@gmail.com>
2024-07-02 21:14:25 -0300

    build: update dependencies and native folders

--- android/app/src/main/kotlin/com/example/calculator/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/calculator/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.felipecastrosales.calculator
+package com.example.calculator
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
